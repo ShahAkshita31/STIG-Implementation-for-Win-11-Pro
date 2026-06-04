2aa621b68fb62849d75b7a91c90c471d09fd79ae
Akshita Shah Portfolio <104895726+ShahAkshita31@users.noreply.github.com>
2026-06-04 14:16:41 -0400

    Update WN11-SO-000070-remediation.ps1

--- WN11-SO-000070/WN11-SO-000070-remediation.ps1
+++ WN11-SO-000070/WN11-SO-000070-remediation.ps1
@@ -1,1 +1,29 @@
+<#
+.SYNOPSIS
+    This PowerShell script prevents IP source routing by configuring Disable IP Source Routing to the highest protection level.
+
+.NOTES
+    Author          : Akshita Shah
+    LinkedIn        : linkedin.com/in/akshita-shah-06b924101
+    GitHub          : github.com/ShahAkshita31
+    Date Created    : 2026-06-04
+    Last Modified   : 2026-06-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-SO-000070
+    Documentation: https://stigaview.com/products/win11/v2r7/WN11-SO-000070/
+
+.TESTED ON
+    Date(s) Tested  : 2026-06-04
+    Tested By       :  Akshita Shah
+    Systems Tested  : Windows 11 Pro (24H2)
+    PowerShell Ver. : 5.1, 7.4
+
+.USAGE
+    Run this script as Administrator.
+    Example syntax:
+    PS C:\> .\WN11-SO-000070-remediation.ps1
+#>
+# Define the Registry path and value details
 
