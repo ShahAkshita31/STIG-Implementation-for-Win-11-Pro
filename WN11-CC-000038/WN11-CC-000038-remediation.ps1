d7cab20009ff9bc0f02ef4da33ab5ede58a92a5f
Akshita Shah Portfolio <104895726+ShahAkshita31@users.noreply.github.com>
2026-06-05 13:52:55 -0400

    Update WN11-CC-000038-remediation.ps1

--- WN11-CC-000038/WN11-CC-000038-remediation.ps1
+++ WN11-CC-000038/WN11-CC-000038-remediation.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+    This PowerShell script prevents IP source routing by configuring Disable IP Source Routing to the highest protection level.
+
+.NOTES
+    Author          : Akshita Shah
+    LinkedIn        : linkedin.com/in/akshita-shah-06b924101
+    GitHub          : github.com/ShahAkshita31
+    Date Created    : 2026-06-04
+    Last Modified   : 2026-06-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000038
+    Documentation: https://stigaview.com/products/win11/v2r7/WN11-CC-000038/
+
+.TESTED ON
+    Date(s) Tested  : 2026-06-04
+    Tested By       :  Akshita Shah
+    Systems Tested  : Windows 11 Pro (24H2)
+    PowerShell Ver. : 5.1, 7.4
+
+.USAGE
+    Run this script as Administrator.
+    Example syntax:
+    PS C:\> .\WN11-CC-000038-remediation.ps1
+#>
+# Define the Registry path and value details
 $regPath = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest"
 $regName = "UseLogonCredential"
 $desiredValue = 0
